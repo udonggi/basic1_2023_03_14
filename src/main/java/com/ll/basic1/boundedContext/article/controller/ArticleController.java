ab40ff80bbd5164cfa85a526c6919773936f01a8
udonggi <dongki1882@naver.com>
2023-03-17 15:31:02 +0900

    auto date insert

--- src/main/java/com/ll/basic1/boundedContext/article/controller/ArticleController.java
+++ src/main/java/com/ll/basic1/boundedContext/article/controller/ArticleController.java
@@ -3,6 +3,7 @@ package com.ll.basic1.boundedContext.article.controller;
 import com.ll.basic1.base.rsData.RsData;
 import com.ll.basic1.boundedContext.article.entity.Article;
 import com.ll.basic1.boundedContext.article.repository.ArticleRepository;
+import com.ll.basic1.boundedContext.article.service.ArticleService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,22 +17,22 @@ import java.time.LocalDateTime;
 @RequiredArgsConstructor //필드 중 final 붙은 것만 인자로 입력받는 생성자 생성
 public class ArticleController {
 
-    private final ArticleRepository articleRepository;
+    private final ArticleService articleService;
 
     @GetMapping("/write")
     @ResponseBody
     public RsData write(String title, String body){
+        if(title==null || title.trim().length() == 0){
+            return RsData.of("F-1", "title이 입력되지 않았습니다");
+        }
+        if(body==null || body.trim().length() == 0){
+            return RsData.of("F-1", "body가 입력되지 않았습니다");
+        }
 
-        Article article = Article.builder()
-                .createDate(LocalDateTime.now())
-                .modifyDate(LocalDateTime.now())
-                .title(title)
-                .body(body)
-                .build();
+        Article createdArticle = articleService.write(title, body);
 
-        articleRepository.save(article); //insert, update
 
-        return RsData.of("S-1","1번",article);
+        return RsData.of("S-1","1번",createdArticle);
 
     }
 }
